e644f7ed4edb7e9767c075d397cea806e7ccd983
With <withjobs@lenta.ru>
2021-02-18 00:21:29 +0300

    WeeNote
    WeeNote is a web app designed for creating, storing and managing your notes.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WeeNote.Models;
+
+namespace WeeNote.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<WeeNote.Models.Note> Note { get; set; }
+    }
+}
---
+++ Models/Note.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeeNote.Models{
+    public class Note
+    {
+        public int Id { get; set; }
+        public string NoteTitle { get; set; }
+        public string NoteText { get; set; }
+        public string Tag { get; set; }
+
+        public Note()
+        {
+
+        }
+    }
+}
